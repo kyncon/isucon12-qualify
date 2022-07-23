cf99fbb3c8a3b6734e986d11fdc1b53dec944ce3
Ryo Kitagawa <kitadrum50@gmail.com>
2022-07-23 12:17:46 +0900

    Fix

--- sql/init.sh
+++ sql/init.sh
@@ -28,7 +28,7 @@ mysql -u"$TENANT_DB_USER" \
   -p"$TENANT_DB_PASSWORD" \
   --host "$TENANT_DB_HOST" \
   --port "$TENANT_DB_PORT" \
-  $"TENANT_DB_NAME" < ./tenant/10_schema.sql
+  "$TENANT_DB_NAME" < ./tenant/10_schema.sql
 for db in $( ls ../../initial_data/*.db ); do
   mysql -u"$TENANT_DB_USER" \
     -p"$TENANT_DB_PASSWORD" \
