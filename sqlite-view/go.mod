f766ad77cf8757e1e0dab6fa8556c975e9ac3b61
Ryo Kitagawa <kitadrum50@gmail.com>
2022-07-23 14:59:56 +0900

    Add sqlite-view

---
+++ sqlite-view/go.mod
@@ -0,0 +1,3 @@
+module github.com/kyncon/isucon12-qualify/sqlite-view
+
+go 1.18
---
+++ sqlite-view/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		fmt.Fprintln(os.Stderr, "You should set args")
+		os.Exit(1)
+	}
+
+	if err := run(args[1]); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(1)
+	}
+}
+
+type data struct {
+	Time         time.Time `json:"time"`
+	Statement    string    `json:"statement"`
+	Args         []string  `json:"args"`
+	QueryTime    float64   `json:"query_time"`
+	AffectedRows int       `json:"affected_rows"`
+}
+
+func run(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	sc.Split(bufio.ScanLines)
+
+	dataList := make(map[string][]data)
+	for sc.Scan() {
+		var d data
+		err := json.Unmarshal(sc.Bytes(), &d)
+		if err != nil {
+			return err
+		}
+		dataList[d.Statement] = append(dataList[d.Statement], d)
+	}
+
+	fmt.Println("statement, count, average")
+	for query, dataList := range dataList {
+		fmt.Printf("%s, %d, %f, %f", query, count(dataList), average(dataList))
+	}
+
+	return nil
+}
+
+func average(dataList []data) float64 {
+	return sum(dataList) / float64(count(dataList))
+}
+
+func sum(dataList []data) float64 {
+	s := 0.
+	for _, d := range dataList {
+		s += d.QueryTime
+	}
+	return s
+}
+
+func count(dataList []data) int {
+	return len(dataList)
+}
